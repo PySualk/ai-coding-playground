a7a392d8179ef48f814a3d9b8c4f00a496f23cb4
Klaus <PySualk@users.noreply.github.com>
2026-01-24 11:32:10 +0000

    bootstrap spring boot backend

---
+++ backend/aiplayground/src/main/kotlin/org/sualk/aiplayground/AiplaygroundApplication.kt
@@ -0,0 +1,11 @@
+package org.sualk.aiplayground
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AiplaygroundApplication
+
+fun main(args: Array<String>) {
+	runApplication<AiplaygroundApplication>(*args)
+}
---
+++ backend/aiplayground/src/test/kotlin/org/sualk/aiplayground/AiplaygroundApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.sualk.aiplayground
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AiplaygroundApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
